2895f2d17f4998a58f7870d5508bca3a501d1039
john <john@ubuntu-linux.myguest.virtualbox.org>
2023-08-08 23:24:20 +0300

    Add logger variable

--- stopes/pipelines/asr_bleu/compute_asr_bleu.py
+++ stopes/pipelines/asr_bleu/compute_asr_bleu.py
@@ -1,86 +1,60 @@
 import asyncio
 import logging
-import sacrebleu
-from stopes.pipelines.asr_bleu.configs import AsrBleuConfig
-from tqdm import tqdm
-from stopes.pipelines.asr_bleu.asr_generator import ASRGenerator
-from stopes.pipelines.asr_bleu.transcribe_audio import transcribe_audiofile
-from stopes.pipelines.asr_bleu.retrieve_data import (retrieve_data, retrieve_asr_config)                                                
-import hydra
 
+from stopes.pipelines.asr_bleu.configs import AsrBleuConfig
+from stopes.pipelines.asr_bleu.asr_generator import retrieve_asr_config
+from stopes.pipelines.asr_bleu.transcribe_audio import transcribe_audio
+from stopes.pipelines.asr_bleu.retrieve_data import retrieve_data
+
+
+import hydra
+import sacrebleu
 
 logger = logging.getLogger("asr_bleu")
-
 
 class AsrBleu:
     def __init__(self, config: AsrBleuConfig):
         self.config = config
         self.launcher = hydra.utils.instantiate(self.config.launcher)
 
+
     async def run(self):
         # 1. Retrieve ASR configuration 
+        logger.info("Setting up ASR model...")
+        asr_config = retrieve_asr_config(self.config.corpora.lang, self.config.corpora.asr_version, 
+                                         json_path="/home/john/Desktop/STOPES/stopes/stopes/pipelines/asr_bleu/conf/asr_models/asr_model_cfgs.json")
 
-        asr_config = retrieve_asr_config(self.config.corpora.lang, self.config.corpora.asr_version, json_path="/home/john/Desktop/STOPES/stopes/stopes/pipelines/asr_bleu/conf/asr_models/asr_model_cfgs.json")
-        asr_model = ASRGenerator(asr_config)
-        
-        # 2. Evaluation data
-
-        eval_manifest = await retrieve_data(
-              [(self.config.corpora.audio_dirpath, self.config.corpora.reference_path)], 
-                self.launcher,
-                self.config.corpora.audio_format,
-                self.config.corpora.reference_format,
-                self.config.corpora.reference_tsv_column
+        # 2. Compose evaluation data.
+        logger.info("Composing evaluation data...")
+        eval_manifests = await retrieve_data([
+                (self.config.corpora.audio_dirpath, 
+                 self.config.corpora.reference_path,
+                 self.config.corpora.audio_format,
+                 self.config.corpora.reference_format,
+                 self.config.corpora.reference_tsv_column,)
+            ], 
+            self.launcher,
         )
 
         # 3. Transcribe audio predictions and compute BLEU score.
-        prediction_transcripts = []
-        for _, eval_pair in tqdm(
-            eval_manifest.iterrows(),
-            desc="Transcribing predictions",
-            total=len(eval_manifest),
-        ):
-            transcription = await transcribe_audiofile(asr_model,eval_pair.prediction)
-            prediction_transcripts.append(transcription.lower())
+        logger.info("Transcribing audio predictions...")
+        transcribed_audio = await transcribe_audio(
+            eval_manifests,
+            self.launcher,
+            asr_config,
+        )
 
-        if self.config.corpora.lang == "hok":
-            prediction_transcripts = [
-                merge_tailo_init_final(text) for text in prediction_transcripts
-            ]
+        # 4. Compute BLEU score
+        logger.info("Computing BLEU scores...")
+        bleu_scores = []
+        for i, prediction_transcripts in enumerate(transcribed_audio):
+            references = eval_manifests[i]["reference"]
+            bleu_score = sacrebleu.corpus_bleu(prediction_transcripts, [references])
+            bleu_scores.append(bleu_score)
+            print(bleu_score)
 
-        references = eval_manifest["reference"].tolist()
-        bleu_score = sacrebleu.corpus_bleu(prediction_transcripts, [references])
+        return transcribed_audio, bleu_scores
 
-        print(bleu_score)
-
-        
-        return prediction_transcripts, bleu_score   
-
-def merge_tailo_init_final(text):
-    """
-    Hokkien ASR hypothesis post-processing.
-    """
-    sps = text.strip().split()
-    results = []
-    last_syllable = ""
-    for sp in sps:
-        if sp == "NULLINIT" or sp == "nullinit":
-            continue
-        last_syllable += sp
-        if sp[-1].isnumeric():
-            results.append(last_syllable)
-            last_syllable = ""
-    if last_syllable != "":
-        results.append(last_syllable)
-    return " ".join(results)
-
-
-def remove_tone(text):
-    """
-    Used for tone-less evaluation of Hokkien
-    """
-    return " ".join([t[:-1] for t in text.split()])
- 
 
 @hydra.main(config_path="conf", config_name="asr_bleu")
 def main(config: AsrBleuConfig) -> None:
