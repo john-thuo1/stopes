6134108e4e5be9f938378c996d358191e6c21889
john <john@ubuntu-linux.myguest.virtualbox.org>
2023-08-09 17:20:27 +0300

    Add configs

--- stopes/pipelines/asr_bleu/configs.py
+++ stopes/pipelines/asr_bleu/configs.py
@@ -6,7 +6,6 @@ class CorporaConfig:
     audio_dirpath: str
     reference_path: str
     reference_format: str
-    # launcher: tp.Dict[str, tp.Any]
     asr_version: str = "oct22"
     reference_tsv_column: str = None
     audio_format: str = "n_pred.wav"
