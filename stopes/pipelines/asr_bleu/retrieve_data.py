afb0151f922bb1e2628ffdf6455e455e912cbbe5
john <john@ubuntu-linux.myguest.virtualbox.org>
2023-08-14 05:44:31 +0300

    Modify pipeline configurations

--- stopes/pipelines/asr_bleu/retrieve_data.py
+++ stopes/pipelines/asr_bleu/retrieve_data.py
@@ -7,9 +7,11 @@ from pathlib import Path
 from glob import glob
 from omegaconf.omegaconf import MISSING
 
+from stopes.core import utils
 from stopes.core.launcher import Launcher
 from stopes.core.stopes_module import Requirements, StopesModule
-from stopes.core.utils import open
+from stopes.pipelines.asr_bleu.configs import CorporaConfig
+
 
 @dataclass
 class RetrieveDataJob:
@@ -18,17 +20,17 @@ class RetrieveDataJob:
     audio_format: str = MISSING
     reference_format: str = MISSING
     reference_tsv_column: tp.Optional[str] = None
+    lang: str = MISSING
+    asr_version: str = MISSING
+
 
 @dataclass
 class RetrieveDataConfig:
-    output_dir: Path
     retrieve_data_jobs: tp.List[RetrieveDataJob] = MISSING
 
+
 class RetrieveData(StopesModule):
-    def __init__(self, 
-                 config: RetrieveDataConfig
-    ):
-        
+    def __init__(self, config: RetrieveDataConfig):
         super().__init__(config=config, config_class=RetrieveDataConfig)
 
     def array(self):
@@ -42,24 +44,33 @@ class RetrieveData(StopesModule):
             cpus_per_task=1,
             timeout_min=24 * 60,
         )
-    
-    def _extract_audio_for_eval(self, audio_dirpath: str, audio_format: str):
+
+    def _extract_audio_for_eval(
+        self,
+        audio_dirpath: str,
+        audio_format: str
+    ) -> tp.List[str]:
         """Extract audio file paths for subsequent transcription"""
         if audio_format == "n_pred.wav":
             """
-            The assumption here is that 0_pred.wav corresponds to the reference at line position 0 from the reference manifest
+            The assumption here is that 0_pred.wav corresponds to the reference
+            at line position 0 from the reference manifest
             """
             audio_list = []
-            audio_fp_list = glob((Path(audio_dirpath) / "*_pred.wav").as_posix())
+            audio_fp_list = glob((Path(audio_dirpath)
+                                  / "*_pred.wav").as_posix())
             audio_fp_list = sorted(
-                audio_fp_list, key=lambda x: int(os.path.basename(x).split("_")[0])
+                audio_fp_list,
+                key=lambda x: int(os.path.basename(x).split("_")[0])
             )
             for i in range(len(audio_fp_list)):
                 try:
-                    audio_fp = (Path(audio_dirpath) / f"{i}_pred.wav").as_posix()
+                    audio_fp = (Path(audio_dirpath)
+                                / f"{i}_pred.wav").as_posix()
                     assert (
                         audio_fp in audio_fp_list
-                    ), f"{Path(audio_fp).name} does not exist in {audio_dirpath}"
+                    ), f"{Path(audio_fp).name} \
+                       does not exist in {audio_dirpath}"
                 except AssertionError:
                     # check the audio with random speaker
                     audio_fp = Path(audio_dirpath) / f"{i}_spk*_pred.wav"
@@ -74,35 +85,54 @@ class RetrieveData(StopesModule):
             raise NotImplementedError
 
         return audio_list
-    
-    def _extract_text_for_eval(self, references_filepath: str, reference_format: str, reference_tsv_column: str = None):
+
+    def _extract_text_for_eval(
+        self,
+        references_filepath: str,
+        reference_format: str,
+        reference_tsv_column: str = None
+    ) -> tp.List[str]:
         """Extract sentences for reference"""
         if reference_format == "txt":
-            reference_sentences = open(references_filepath, "r").readlines()
-            reference_sentences = [l.strip() for l in reference_sentences]
+            reference_sentence_file = utils.open(references_filepath, "r")
+            reference_sentences = [
+                line.strip() for line in reference_sentence_file.readlines()
+            ]
         elif reference_format == "tsv":
             tsv_df = pd.read_csv(references_filepath, sep="\t", quoting=3)
             reference_sentences = tsv_df[reference_tsv_column].to_list()
-            reference_sentences = [l.strip() for l in reference_sentences]
+            reference_sentences = [
+                line.strip() for line in reference_sentences
+            ]
         else:
             raise NotImplementedError
 
         return reference_sentences
 
-    def run(self,
-            iteration_value: tp.Optional[tp.Any] = None,
-            iteration_index: int = 0,
-    ) -> tp.Dict[str, tp.List]:
+    def run(
+        self,
+        iteration_value: tp.Optional[tp.Any] = None,
+        iteration_index: int = 0,
+    ) -> tp.Tuple[tp.Dict[str, tp.List], str, str]:
         """Retrieves data for each RetrieveDataJob"""
         assert iteration_value is not None, "iteration value is null"
-        self.logger = logging.getLogger("stopes.asr_bleu.retrieve_data")
+        self.logger = logging.getLogger("stopes.asr_bleu.prepare_data")
 
-        self.logger.info(f"Retrieving audio data from {iteration_value.audio_path}")
-        audio_list = self._extract_audio_for_eval(iteration_value.audio_path, iteration_value.audio_format)
+        self.logger.info(
+            f"Retrieving audio data from {iteration_value.audio_path}"
+        )
+        audio_list = self._extract_audio_for_eval(
+            iteration_value.audio_path,
+            iteration_value.audio_format
+        )
 
-        self.logger.info(f"Retrieving text data from {iteration_value.reference_path}")
+        self.logger.info(
+            f"Retrieving text data from {iteration_value.reference_path}"
+        )
         reference_sentences = self._extract_text_for_eval(
-            iteration_value.reference_path, iteration_value.reference_format, iteration_value.reference_tsv_column
+            iteration_value.reference_path,
+            iteration_value.reference_format,
+            iteration_value.reference_tsv_column
         )
 
         eval_manifest = {
@@ -110,32 +140,35 @@ class RetrieveData(StopesModule):
             "reference": reference_sentences,
         }
 
-        return eval_manifest
+        return (
+            eval_manifest,
+            iteration_value.lang,
+            iteration_value.asr_version
+        )
 
 
 async def retrieve_data(
-    # self.config.corpora: 
-    datasets: tp.List[tp.Tuple[str, str, str, str, str]],
+    corpora_conf: CorporaConfig,
     launcher: Launcher,
-    output_dir: Path,
-):
+) -> tp.List[tp.Tuple[tp.Dict[str, tp.List], str, str]]:
     """
     Retrieve data for transcription
-    Returns a list of type dict[str, list]
-    Datasets are a 5 tuple: (audio_path, reference_path, audio_format, reference_format, reference_tsv_column)
+    Returns a list of 3 tuples: (eval_manifest, lang, asr_version)
     """
+    datasets = corpora_conf.datasets
     retrieve_data_jobs = [
         RetrieveDataJob(
-            audio_path=dataset[0], 
-            reference_path=dataset[1], 
-            audio_format=dataset[2], 
-            reference_format=dataset[3], 
-            reference_tsv_column=dataset[4],
-        ) for dataset in datasets
+            audio_path=datasets[corpus].audio_dirpath,
+            reference_path=datasets[corpus].reference_path,
+            audio_format=datasets[corpus].audio_format,
+            reference_format=datasets[corpus].reference_format,
+            reference_tsv_column=datasets[corpus].reference_tsv_column,
+            lang=datasets[corpus].lang,
+            asr_version=datasets[corpus].asr_version,
+        ) for corpus in datasets
     ]
     retrieve_data_module = RetrieveData(
         RetrieveDataConfig(
-            output_dir=output_dir,
             retrieve_data_jobs=retrieve_data_jobs,
         )
     )
